015f74d622b0e1f2372bef59ddffac1055dd3dd1
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-05-17 22:45:32 +0700

    Create view-python2.py

---
+++ view-python2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('Heart Failure Prediction Datasets and Charts')
+st.markdown('The webiste was the second project of MATLAB & Python part of the Business IT2 course. This page was prepared by Group 10 in Thursday morning class under the guidance of Dr. Do Duc Tan.')
+st.markdown('Group members: Le Phuong Linh, Nguyen Minh Tri, Nguyen Nhu Ngoc, Pham Dinh Khanh Ngoc')
+st.header('Original dataset')
