27d84e74ec409ca4c221a9bd50e692aa96c13e03
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-05-23 14:13:49 +0700

    Create HOMEPAGE.py

---
+++ HOMEPAGE.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import streamlit_lottie as st_lottie
+from streamlit_extras.colored_header import colored_header
+from annotated_text import annotated_text
+from streamlit_extras.stoggle import stoggle
+from streamlit_extras.let_it_rain import rain
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_icon="chart_with_upwards_trend", page_title="BUSINESS IT 2 - PYTHON PROJECT 2", layout="wide")
+st.subheader("Group 10")
+st.title("PYTHON 2 - BUSINESS IT 2 Thursday Morning:orange_heart:")
+st.write("Since all members of our group are interested in what causes the most deaths globally, and this dataset appears with various numbers with its reasons, we want to find out the data have an Intuitive view through different types of charts. In the process of finding a data set for the R Studio Project and Python Project, we were impressed by the information “taking an estimated 17.9 million lives each year, which accounts for 31% of all deaths worldwide”, which was a huge statistic. This data set not only is in the field we enjoy but also has a standard structure we need for the project.")
+stoggle(
+    "Group information",
+    """
+    \n 1. Le Phuong Linh - 10323049
+    \n 2. Nguyen Nhu Ngoc - 10223019
+    \n 3. Nguyen Minh Tri - 10623045
+    \n 4. Pham Dinh Khanh Ngoc - 10623033""",
+)
