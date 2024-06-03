23d554c55412388cd60071f614e202ae23036b32
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-03 21:44:06 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -1,5 +1,3 @@
-pip install streamlit-lottie
-
 import streamlit as st 
 import pandas as pd 
 import numpy as np
@@ -23,11 +21,13 @@ stoggle(
     "Group information",
     """
 
- 1. Dinh Ha Tu Van - 10622045
+ 1. Le Phuong Linh - 10323049
 
- 2. Bui Cam Ha Quyen - 10622023
+ 2. Nguyen Minh Tri - 16233
 
- 3. Mai Hong Hanh - 10622014""",
+ 3. Nguyen Nhu Ngoc - 10323019
+
+ 4. Pham Dinh Khanh Ngoc - 10623033""",
 )
 
 st.write("[Accessing our dataset >](https://docs.google.com/spreadsheets/d/1HbBDpeXYXhl3MQU2bZ-YZiHb1Fe2COrT/edit?usp=drive_link&ouid=114022649098492793407&rtpof=true&sd=true)")
@@ -39,60 +39,3 @@ rain(
     animation_length="3",
 )
 
-colored_header(
-    label="Group members introduction",
-    description="Get to know about our group",
-    color_name="light-blue-70",)
-
-tv1, tv2 = st.columns(2)
-with tv1:
-    tuvan = Image.open('3.png')
-    st.image(tuvan)
-with tv2:
-    st.subheader("**Full name: Dinh Ha Tu Van (Group leader)**")
-    st.write("Student ID: 10622045")
-    st.write("Email: 10622045@student.vgu.edu.vn")
-    st.write("Major: Business Administration (BBA)")
-    st.write("Phone number: 077 6209215")
-
-st.write(" ")
-
-hq1, hq2 = st.columns(2)
-with hq1:
-    haquyen = Image.open('2.png')
-    st.image(haquyen)
-with hq2:
-    st.subheader("**Full name: Bui Cam Ha Quyen**")
-    st.write("Student ID: 10622023")
-    st.write("Email: 10622023@student.vgu.edu.vn")
-    st.write("Major: Finance & Accounting (BFA)")
-    st.write("Phone number: 090 8784370")
-
-st.write(" ")
-
-mh1, mh2 = st.columns(2)
-with mh1:
-    maihanh = Image.open('1.png')
-    st.image(maihanh)
-with mh2:
-    st.subheader("**Full name: Mai Hong Hanh**")
-    st.write("Student ID: 10622014")
-    st.write("Email: 10622014@student.vgu.edu.vn")
-    st.write("Major: Business Administration (BBA)")
-    st.write("039 2230636")
-
-st.markdown("---")
-
-st.subheader("💬 Leave us your message!")
-
-st.caption("Let us know if you have any recommendations")
-
-contactform = """<form action="https://formsubmit.co/10622045@student.vgu.edu.vn" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email address" required>
-     <textarea name="message" placeholder="What do you think?"></textarea>
-     <button type="submit">Send</button>
-</form>"""
-
-st.markdown(contactform,unsafe_allow_html=True)
