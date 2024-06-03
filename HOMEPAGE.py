c16404b3c3475fb8fb934ca5fe6bf4b36042967c
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-03 21:49:59 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -14,7 +14,7 @@ from streamlit_extras.let_it_rain import rain
 
 st.set_page_config(page_icon="chart_with_upwards_trend", page_title="BUSINESS IT 2 - PYTHON PROJECT 2", layout="wide")
 st.subheader("Group 10")
-st.title("PYTHON 2 - BUSINESS IT 2 Thursday Morning:orange_heart:")
+st.title("PYTHON 2 - BUSINESS IT 2 Thursday Morning:🧐:")
 st.write("Since all members of our group are interested in what causes the most deaths globally, and this dataset appears with various numbers with its reasons, we want to find out the data have an Intuitive view through different types of charts. In the process of finding a data set for the R Studio project, we were impressed by the information “taking an estimated 17.9 million lives each year, which accounts for 31% of all deaths worldwide”, which was a huge statistic. This data set not only is in the field we enjoy but also has a standard structure we need for the project.")
 
 stoggle(
@@ -30,12 +30,10 @@ stoggle(
  4. Pham Dinh Khanh Ngoc - 10623033""",
 )
 
-st.write("[Accessing our dataset >](https://docs.google.com/spreadsheets/d/1HbBDpeXYXhl3MQU2bZ-YZiHb1Fe2COrT/edit?usp=drive_link&ouid=114022649098492793407&rtpof=true&sd=true)")
-
 rain(
-    emoji="🔍",
+    emoji="❤️",
     font_size=54,
-    falling_speed=5,
-    animation_length="3",
+    falling_speed=3,
+    animation_length="5",
 )
 
