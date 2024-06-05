41ce4dc251aa389be2027db3de2713fe09ed5ea3
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-05 15:08:17 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -1,13 +1,21 @@
+# LOADING PACKAGES
 import streamlit as st 
 import pandas as pd 
 
+# SETTING HOMEPAGE
 st.set_page_config(page_icon="chart_with_upwards_trend", page_title="BUSINESS IT 2 - PYTHON PROJECT 2", layout="wide")
 st.subheader("Group 10")
 st.title("PYTHON 2 - BUSINESS IT 2 Thursday Morning 🧐")
 
 st.write("""
-Since all members of our group are interested in what causes the most deaths globally, and this dataset appears with various numbers with its reasons,  we want to find out the data have an Intuitive view through different types of charts.  In the process of finding a data set for the R Studio project, we were impressed by the information “taking an estimated 17.9 million lives each year,  which accounts for 31% of all deaths worldwide”, which was a huge statistic.  This data set not only is in the field we enjoy but also has a standard structure we need for the project.
+💁🏻 Since all members of our group are interested in what causes the most deaths globally, and this dataset appears with various numbers with its reasons,  we want to find out the data have an Intuitive view through different types of charts.  In the process of finding a data set for the R Studio project, we were impressed by the information “taking an estimated 17.9 million lives each year,  which accounts for 31% of all deaths worldwide”, which was a huge statistic.  This data set not only is in the field we enjoy but also has a standard structure we need for the project.
 """)
-
 st.write("""Group information: Le Phuong Linh - 10323049; Nguyen Minh Tri - 10623045; Nguyen Nhu Ngoc - 10323019; Pham Dinh Khanh Ngoc - 10623033
 """)
+st.write("[Viewing our dataset >](https://docs.google.com/spreadsheets/d/1OFNByvybC163CwUWIFsOyqrDnyug_t4X/edit?usp=sharing&ouid=113809598862321872480&rtpof=true&sd=true)")
+
+#SETTING INTRODUCTION PAGE
+st.set_page_config(page_title="Information about Cardiovascular Disease",page_icon="🫀",layout="wide")
+st.subheader("Business IT 2 | Python 2")
+st.title(':yellow[🤔 What is Cardiovascular Disease?]')
+st.write(" ")
