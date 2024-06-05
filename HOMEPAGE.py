cba70a16144d2bdc9c3678a4eca9f955f262d7a3
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-05 16:31:51 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -1,18 +1,6 @@
 # LOADING PACKAGES
 import pandas as pd
 import streamlit as st
-import seaborn as sns
-import numpy as np
-import json
-import matplotlib.pyplot as plt
-import streamlit_lottie as st_lottie
-import plotly.express as px
-import requests
-from streamlit_extras.colored_header import colored_header
-from annotated_text import annotated_text
-from PIL import Image
-from streamlit_extras.stoggle import stoggle
-from streamlit_extras.let_it_rain import rain
 
 
 # SETTING PAGE
