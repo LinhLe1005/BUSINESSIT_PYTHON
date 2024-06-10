511150a6b25f0fa9783036fc4e6007fb42ba7dd4
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-10 15:07:43 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -92,3 +92,34 @@ plt.tight_layout()
 
 # showing plot
 plt.show()
+
+
+
+
+
+# Define colors
+colors1 = ["#EDCC6F", "#F57893"]
+
+# Create the facet grid plot
+def create_scatterplot():
+    scatterplot = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
+    scatterplot.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
+
+    # Adding label names and title
+    scatterplot.fig.subplots_adjust(top=0.9)
+    scatterplot.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
+    scatterplot.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
+
+    # Setting the size of the plot
+    scatterplot.fig.set_size_inches(10, 6)
+    plt.tight_layout()
+
+    # Return the plot figure
+    return scatterplot.fig
+
+# Streamlit app
+st.title('Heart Disease Analysis')
+
+# Create and display the plot
+fig = create_scatterplot()
+st.pyplot(fig)
