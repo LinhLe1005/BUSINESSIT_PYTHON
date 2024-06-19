230f51fae9be282f5f3544f0a42903ea985583a9
TriNguyen235 <10623045@student.vgu.edu.vn>
2024-06-19 10:08:47 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -95,4 +95,19 @@ with tab1:
 
 with tab2:
   st.header("This is tab 2 test")
-  st.write("Hello world")
+  colors1 = ["#EDCC6F", "#F57893"]
+  fig, ax= plt.subplots()
+  
+  scatterplot = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
+  scatterplot.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
+
+  scatterplot.fig.subplots_adjust(top=0.9)
+  scatterplot.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
+  scatterplot.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
+
+  # setting the size of the plot
+  scatterplot.fig.set_size_inches(10, 6)
+  plt.tight_layout()
+
+  # showing plot
+  st.pyplot(fig)
