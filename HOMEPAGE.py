6946f1b048b244676f013e0bd4b299b2ffb2bd03
TriNguyen235 <10623045@student.vgu.edu.vn>
2024-06-19 09:39:13 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -73,25 +73,25 @@ tab1, tab2, tab3 = st.tabs(["Resting Blood Pressure", "Resting Electrocardiogram
 ### TAB 1: RESTING BLOOD PRESSURE
 with tab1:
 
-# defining colors
-colors1 = ["#EDCC6F", "#F57893"]
+  # defining colors
+  colors1 = ["#EDCC6F", "#F57893"]
 
 
-# creating facet grid
-scatterplot = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
-scatterplot.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
+  # creating facet grid
+  scatterplot = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
+  scatterplot.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
 
-# adding label names and title
-scatterplot.fig.subplots_adjust(top=0.9)
-scatterplot.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
-scatterplot.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
+  # adding label names and title
+  scatterplot.fig.subplots_adjust(top=0.9)
+  scatterplot.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
+  scatterplot.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
 
-# setting the size of the plot
-scatterplot.fig.set_size_inches(10, 6)
-plt.tight_layout()
+  # setting the size of the plot
+  scatterplot.fig.set_size_inches(10, 6)
+  plt.tight_layout()
 
-# showing plot
-plt.show()
+  # showing plot
+  plt.show()
 
 with tab2:
   st.header("This is tab 2 test")
