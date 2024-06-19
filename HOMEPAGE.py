faa9182e31bd0d669866f3048a3f06dd48f394ed
TriNguyen235 <10623045@student.vgu.edu.vn>
2024-06-19 10:28:04 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -98,15 +98,15 @@ with tab2:
   colors1 = ["#EDCC6F", "#F57893"]
   fig, ax= plt.subplots()
   
-  scatterplot = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
-  scatterplot.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
+  ax = sns.FacetGrid(HEART_DATASETS, col='HeartDisease', hue='HeartDisease', palette=colors1, height=6, aspect=1.5)
+  ax.map(sns.regplot, 'Age', 'RestingBP', scatter_kws={'s': 50}, fit_reg=True, marker='o')
 
-  scatterplot.fig.subplots_adjust(top=0.9)
-  scatterplot.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
-  scatterplot.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
+  ax.fig.subplots_adjust(top=0.9)
+  ax.fig.suptitle('Age and Resting Blood Pressure Distribution by Heart Disease Status', fontsize=15, y=1)
+  ax.set_axis_labels("Age (years)", "Resting Blood Pressure (mm Hg)")
 
   # setting the size of the plot
-  scatterplot.fig.set_size_inches(10, 6)
+  ax.fig.set_size_inches(10, 6)
   plt.tight_layout()
 
   # showing plot
