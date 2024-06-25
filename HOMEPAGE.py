14aa148e7f5a2719d64db882b5dbf9d9efacf125
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-26 02:07:50 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -89,7 +89,7 @@ with tab1:
     # Plotting chart
     fig1 = px.scatter(filtered_data, x='Age', y='RestingBP', color=rank, title=f"Age vs Resting Blood Pressure ({age_type})",
                      labels={'Age': 'Age (years)', 'RestingBP': 'Resting Blood Pressure (mm Hg)', rank: rank}, 
-                      color_discrete_map={"HeartDisease": "#EDCC6F", "ExerciseAngina": "#F57893", "FastingBS": "#EDCC6F"})
+                      color_discrete_map={"HeartDisease": "#6FED84", "ExerciseAngina": "#F57893", "FastingBS": "#6F89ED"})
 
     # Display chart
     st.plotly_chart(fig1)
