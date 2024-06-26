0c12a3a94abaeb87ff67990413635caa0494a354
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-26 16:49:45 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -132,11 +132,10 @@ with tab2:
     col1, col2 = st.columns([7, 4])
     with col1:
          st.write("""
-         :green[**Resting Electrocardiogram (ECG) results**] differentiate cardiovascular disease (CVD) from normal heart function. 
-         Patients with CVD often exhibit abnormal patterns like ST-segment changes, T-wave inversions, and abnormal Q waves, indicating myocardial issues and arrhythmias such as atrial fibrillation. 
-         In contrast, healthy individuals typically display regular sinus rhythm and normal ECG parameters. 
-         ECG serves as a critical diagnostic tool, identifying cardiac abnormalities in CVD patients and confirming normal heart function in others. 
-         These distinctions are illustrated in the :orange[*boxplot charts*] below, depicting the relationship between *achieved maximum heart rate, resting ECG results, and the presence of heart disease* across three different variables.
+         :green[**Resting Electrocardiogram (ECG) results**] distinguish cardiovascular disease (CVD) from normal heart function. 
+         Patients with CVD often show abnormal patterns like ST-segment changes, T-wave inversions, and abnormal Q waves, indicating myocardial issues and arrhythmias. 
+         Healthy individuals typically have regular sinus rhythm and normal ECG parameters. ECG is crucial for diagnosing cardiac abnormalities in CVD patients and confirming normal heart function in others. 
+         These differences are visualized in :orange[*boxplot charts*] below, showing the relationship between *maximum heart rate, resting ECG results, and heart disease presence* across three variables.
          """)
     with col2:
          heartdisease_type = st.radio("💁🏽‍♀️ You're interested in examining the data for", ["HeartDisease", "Normal"], key="cate", disabled=st.session_state.disabled)
