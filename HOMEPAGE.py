dd84184c1193cafb12609989d9cd4618f32b4d81
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-26 07:45:11 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -8,7 +8,7 @@ HEART_DATASETS = pd.read_csv('HEART_DATASETS.csv')
 st.set_page_config(page_icon="chart_with_upwards_trend", page_title="BUSINESS IT 2 - PYTHON PROJECT 2", layout="wide")
 with st.container():
   st.subheader("Group 10")
-  st.title(":red[PYTHON 2 - BUSINESS IT 2 Thursday Morning 🧐]")
+  st.title(":blue[PYTHON 2 - BUSINESS IT 2 Thursday Morning 🧐]")
   st.write("""💁🏻 Since all members of our group are interested in what causes the most deaths globally, and this dataset appears with various numbers with its reasons,  we want to find out the data have an Intuitive view through different types of charts. In the process of finding a data set for the R Studio project, we were impressed by the information “taking an estimated 17.9 million lives each year,  which accounts for 31% of all deaths worldwide”, which was a huge statistic.  This data set is not only in the field we enjoy but also has a standard structure we need for the project.
 """)
 
