8f6fe58c3ba405655d1dc779606cdc226152371b
LinhLe1005 <10323049@student.vgu.edu.vn>
2024-06-27 10:30:16 +0700

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -65,7 +65,6 @@ with st.container():
         \n - :green[**Sex:**] Sex of the patient [M: Male, F: Female]
         \n - :green[**ChestPainType:**] Chest pain type [TA: Typical Angina, ATA: Atypical Angina, NAP: Non-Anginal Pain, ASY: Asymptomatic]
         \n - :green[**RestingBP:**] Resting blood pressure [mm Hg]
-        \n - :green[**Cholesterol:**] Serum cholesterol [mm/dl]
         \n - :green[**FastingBS:**] Fasting blood sugar [1: if FastingBS > 120 mg/dl, 0: Otherwise]
         \n - :green[**RestingECG:**] Resting electrocardiogram results [Normal: Normal, ST: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV), LVH: showing probable or definite left ventricular hypertrophy by Estes' criteria]
         \n - :green[**MaxHR:**] Maximum heart rate achieved [Numeric value between 60 and 202]
